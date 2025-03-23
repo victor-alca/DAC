37cfb9e70108530b240041d55de9c34a82d69071
Felipe <felps@gmail.com>
2025-03-23 01:22:51 -0300

    tela inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { TelaInicialClienteComponent } from './tela-inicial-cliente/tela-inicial-cliente.component';
+import { VerReservaComponent } from './ver-reserva/ver-reserva.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TelaInicialClienteComponent,
+    VerReservaComponent
   ],
   imports: [
     BrowserModule,
