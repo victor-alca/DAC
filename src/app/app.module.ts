42cb958b46db4181526dacd3b72376350871fe9e
Lyandra Lisboa <lyandralisboa08@gmail.com>
2025-04-01 20:32:09 -0300

    feat: RF05 - Comprar de milhas e ajustes em RF01 E RF02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { PhoneValidatorDirective } from './shared/directives/phone-validator.dir
 import { EmailValidatorDirective } from './shared/directives/email-validator.directive';
 import { CpfValidatorDirective } from './shared/directives/cpf-validator.directive';
 import { provideNgxMask } from 'ngx-mask';
+import { BuyMilesComponent } from './pages/buy-miles/buy-miles.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { provideNgxMask } from 'ngx-mask';
     EmployeeDashboardComponent,
     BoardingConfirmationComponent,
     CustomerHomeComponent,
-    ViewBookingComponent
+    ViewBookingComponent,
+    BuyMilesComponent
   ],
   imports: [
     BrowserModule,
