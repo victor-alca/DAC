ac3f57409e9a8c870e03441fe9248bc8452a82aa
victor-alca <54454708+victor-alca@users.noreply.github.com>
2025-04-23 22:20:46 -0300

    feat: Integrando dados do localStorage em alguns componentes e melhorias na estrutura de algumas páginas

--- src/app/services/booking.service.ts
+++ src/app/services/booking.service.ts
@@ -77,42 +77,54 @@ export class BookingService {
     const bookings: Booking[] = [
       // Reservas para o primeiro voo
       new Booking(
-        1,
-        this.getFlightDetails('1')!, // Busca o voo com ID '1'
-        new Date(now.getTime() + 1 * 60 * 60 * 1000), // 1 hora a partir de agora
-        BookingStatus.CREATED
+      1,
+      this.getFlightDetails('1')!, // Busca o voo com ID '1'
+      new Date(now.getTime() + 1 * 60 * 60 * 1000), // 1 hora a partir de agora
+      BookingStatus.CREATED,
+      150, // Dinheiro gasto
+      500  // Milhas gastas
       ),
       new Booking(
-        2,
-        this.getFlightDetails('1')!, // Busca o voo com ID '1'
-        new Date(now.getTime() + 2 * 60 * 60 * 1000), // 2 horas a partir de agora
-        BookingStatus.CHECK_IN
+      2,
+      this.getFlightDetails('1')!, // Busca o voo com ID '1'
+      new Date(now.getTime() + 2 * 60 * 60 * 1000), // 2 horas a partir de agora
+      BookingStatus.CHECK_IN,
+      200, // Dinheiro gasto
+      600  // Milhas gastas
       ),
       // Reservas para o segundo voo
       new Booking(
-        3,
-        this.getFlightDetails('2')!, // Busca o voo com ID '2'
-        new Date(now.getTime() + 12 * 60 * 60 * 1000), // 12 horas a partir de agora
-        BookingStatus.CREATED
+      3,
+      this.getFlightDetails('2')!, // Busca o voo com ID '2'
+      new Date(now.getTime() + 12 * 60 * 60 * 1000), // 12 horas a partir de agora
+      BookingStatus.CREATED,
+      300, // Dinheiro gasto
+      800  // Milhas gastas
       ),
       new Booking(
-        4,
-        this.getFlightDetails('2')!, // Busca o voo com ID '2'
-        new Date(now.getTime() + 14 * 60 * 60 * 1000), // 14 horas a partir de agora
-        BookingStatus.CHECK_IN
+      4,
+      this.getFlightDetails('2')!, // Busca o voo com ID '2'
+      new Date(now.getTime() + 14 * 60 * 60 * 1000), // 14 horas a partir de agora
+      BookingStatus.CHECK_IN,
+      250, // Dinheiro gasto
+      700  // Milhas gastas
       ),
       // Reservas para o terceiro voo
       new Booking(
-        5,
-        this.getFlightDetails('3')!, // Busca o voo com ID '3'
-        new Date(now.getTime() + 72 * 60 * 60 * 1000), // 72 horas a partir de agora
-        BookingStatus.CANCELED
+      5,
+      this.getFlightDetails('3')!, // Busca o voo com ID '3'
+      new Date(now.getTime() + 72 * 60 * 60 * 1000), // 72 horas a partir de agora
+      BookingStatus.CANCELED,
+      400, // Dinheiro gasto
+      1000  // Milhas gastas
       ),
       new Booking(
-        6,
-        this.getFlightDetails('3')!, // Busca o voo com ID '3'
-        new Date(now.getTime() + 74 * 60 * 60 * 1000), // 74 horas a partir de agora
-        BookingStatus.CREATED
+      6,
+      this.getFlightDetails('3')!, // Busca o voo com ID '3'
+      new Date(now.getTime() + 74 * 60 * 60 * 1000), // 74 horas a partir de agora
+      BookingStatus.CREATED,
+      350, // Dinheiro gasto
+      900  // Milhas gastas
       ),
     ];
 
