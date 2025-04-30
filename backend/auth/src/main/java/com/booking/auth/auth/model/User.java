7e49d0159daa0489156cd66a7d87a4351139f126
Lyandra Lisboa <lyandralisboa08@gmail.com>
2025-04-30 16:57:34 -0300

    feat: create models

---
+++ backend/auth/src/main/java/com/booking/auth/auth/model/User.java
@@ -0,0 +1,69 @@
+package com.booking.auth.auth.model;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "auth")
+public class User {
+  @Id
+  private String id;
+
+  @NotBlank
+  @Size(max = 120)
+  private String email;
+
+  @NotBlank
+  @Size(max = 120)
+  private String password;
+
+  @NotBlank
+  @Size(max = 120)
+  private String type;
+
+  public User() {
+    super();
+  }
+
+	public User(String id, String email, String password, String type) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.type = type;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setLogin(String login) {
+		this.email = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
