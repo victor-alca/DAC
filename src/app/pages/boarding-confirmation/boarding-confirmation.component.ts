b1fd37c1198cfac1068a6e6063b795e094e4b8fa
Luan Pinheiro <luluka2909@gmail.com>
2025-06-21 20:04:19 -0300

    integração de confirmação de embarque

--- src/app/pages/boarding-confirmation/boarding-confirmation.component.ts
+++ src/app/pages/boarding-confirmation/boarding-confirmation.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { BookingService } from '../../services/booking.service';
 import { Booking } from '../../shared/models/booking/booking.model';
 import { BookingStatus } from '../../shared/models/booking/booking-status.enum';
+import { CreateBookingDTO } from '../../shared/dtos/createBookingDTO';
+import { firstValueFrom } from 'rxjs';
+import { CreateBookingResponseDTO } from '../../shared/dtos/createBookingResponseDTO';
 
 @Component({
   selector: 'app-boarding-confirmation',
@@ -15,7 +18,7 @@ export class BoardingConfirmationComponent {
 
   constructor(private bookingService: BookingService) {}
 
-  confirmBoarding(): void {
+  async confirmBoarding(): Promise<void> {
     this.errorMessage = null;
     this.successMessage = null;
 
@@ -24,24 +27,41 @@ export class BoardingConfirmationComponent {
       return;
     }
 
-    const bookingId = parseInt(this.reservationCode, 10);
-    //const booking = this.bookingService.getById(bookingId);
+    const booking : CreateBookingResponseDTO = await this.loadBooking()
 
-    // if (!booking) {
-    //   this.errorMessage = 'Código de reserva inválido ou não encontrado.';
-    //   return;
-    // }
 
-    // if (booking.status !== BookingStatus.CHECK_IN) {
-    //   this.errorMessage = 'A reserva não está no estado CHECK-IN.';
-    //   return;
-    // }
+    if (!booking) {
+      this.errorMessage = 'Código de reserva inválido ou não encontrado.';
+      return;
+    }
 
-    // if (confirm(`Tem certeza que deseja confirmar o embarque da reserva ${this.reservationCode}?`)) {
-    //   booking.status = BookingStatus.SHIPPED; // Estado EMBARCADO
-    //   this.bookingService.update(booking);
-    //   this.successMessage = `Reserva ${this.reservationCode} confirmada com sucesso!`;
-    //   this.reservationCode = ''; // Limpa o campo após sucesso
-    // }
+    if (booking.estado !== this.bookingService.getBookingStatusText(BookingStatus.CHECK_IN)) {
+      this.errorMessage = 'A reserva não está no estado CHECK-IN.';
+      return;
+    }
+
+    if (confirm(`Tem certeza que deseja confirmar o embarque da reserva ${this.reservationCode}?`)) {
+      booking.estado = this.bookingService.getBookingStatusText(BookingStatus.SHIPPED); // Estado EMBARCADO
+      this.bookingService.update(booking);
+      this.successMessage = `Reserva ${this.reservationCode} confirmada com sucesso!`;
+      this.reservationCode = ''; // Limpa o campo após sucesso
+    }
   }
+
+    async loadBooking(): Promise<CreateBookingResponseDTO> {
+       try {
+      const resp = await firstValueFrom(this.bookingService.getById(this.reservationCode));
+  
+      if (resp != null) {
+        return resp
+      } else {
+        console.log("Nenhuma reserva encontrada");
+        throw new Error("Nenhuma reserva encontrada")
+      }
+  
+    } catch (e) {
+      console.log("Erro ao buscar reservas:", e);
+      throw (e)
+    }
+    }
 }
